3b7ec24d709c4c08f60dd9f00da64e14b863980f
Felipe <felipe@toke.com>
2022-10-22 01:04:08 -0300

    Removed print

--- lib/util/host_checker_util.dart
+++ lib/util/host_checker_util.dart
@@ -1,6 +1,5 @@
 Uri? testHosts(List<String?> hosts) {
   for (final host in hosts) {
-    print('host: $host');
     if (host != null && host.isNotEmpty) {
       final uri = Uri.tryParse(host);
       if (uri != null && uri.host.isNotEmpty) {
