05c16b16d6d944f41ea3865222acf67d372845ef
Felipe <felipe@toke.com>
2022-12-01 16:23:03 -0300

    Updated OpenStoreWidget

--- lib/features/widgets/open_store_widget.dart
+++ lib/features/widgets/open_store_widget.dart
@@ -9,9 +9,20 @@ class OpenStoreWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () => TargetPlatform.android == defaultTargetPlatform ? launchUrlString(googlePlayStoreUrl, mode: LaunchMode.externalApplication) : null,
-      child: SvgPicture.asset(TargetPlatform.android == defaultTargetPlatform ? androidPng : iOSSvg, height: 60, fit: BoxFit.fitHeight),
+    return Column(
+      children: [
+        if (TargetPlatform.iOS != defaultTargetPlatform)
+          InkWell(
+            onTap: () => launchUrlString(googlePlayStoreUrl, mode: LaunchMode.externalApplication),
+            child: SvgPicture.asset(TargetPlatform.android == defaultTargetPlatform ? androidPng : iOSSvg, height: 60, fit: BoxFit.fitHeight),
+          ),
+        // TODO: Add iOS store link
+        // if (TargetPlatform.android != defaultTargetPlatform)
+        //   InkWell(
+        //     onTap: null,
+        //     child: SvgPicture.asset(TargetPlatform.android == defaultTargetPlatform ? androidPng : iOSSvg, height: 60, fit: BoxFit.fitHeight),
+        //   ),
+      ].map((e) => Padding(padding: const EdgeInsets.symmetric(vertical: 8), child: e)).toList(),
     );
   }
 }
